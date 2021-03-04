e6470e1f80f62c6fb84a6b92b8bf6f433a038b37
Eray Durak <eraydurak1@outlook.com>
2021-03-04 14:04:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import LandingPage from "./LandingPage/LandingPage";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Game from "./Game/Game";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route  path="/game" component={Game} />
+        <Route exact path="/" component={LandingPage} />
+      </Switch>
+    </Router>
   );
 }
 
